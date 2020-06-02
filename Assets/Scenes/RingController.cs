61a521e993d15bed6d6a170a1e16813d10283a0d
José Negrão <joseolimpionegrao@gmail.com>
2020-06-03 00:50:40 +0100

    02

---
+++ Assets/Scenes/RingController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class RingController : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject doorGO;
+    public GameObject ringGO;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DoorControl(); alterar para checar 
+        RingControl();
+    }
+
+    void DoorControl()
+    {
+        doorGO.transform.Rotate(0, 0, (rotationSpeed * Time.deltaTime));
+    }
+    void RingControl()
+    {
+        ringGO.transform.Rotate(0, 0, (rotationSpeed * Time.deltaTime));
+    }
+}
